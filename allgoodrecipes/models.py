915bdd4ab2da2cbbf036dff2fe924e021588447d
allumen <2356067R@student.gla.ac.uk>
2019-02-28 19:15:55 +0100

    Revert "include allgoodrecipes.urls in project urls"
    This reverts commit e28694e74d49dd5a58c11ff2831e51ff2e4679dd.

--- allgoodrecipes/models.py
+++ allgoodrecipes/models.py
@@ -1,13 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    # link UserProfile to django User model instance
-    user = models.OneToOneField(User)
-    
-    # additional attributes
-    #
-    picture = models.ImageField(upload_to='profile_images', blank=True)
-    
-    def __str__(self):
-        return self.user.username
+# Create your models here.
