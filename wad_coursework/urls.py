e28694e74d49dd5a58c11ff2831e51ff2e4679dd
allumen <2356067R@student.gla.ac.uk>
2019-02-28 19:06:22 +0100

    include allgoodrecipes.urls in project urls

--- wad_coursework/urls.py
+++ wad_coursework/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from allgoodrecipes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.index, name='index')
+    path('/', include('allgoodrecipes.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
