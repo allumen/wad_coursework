00c0d90e4c8e37e8ea1d028c6e6bb471151b36f7
allumen <2356067R@student.gla.ac.uk>
2019-03-07 16:21:15 +0100

    add url mappings for profil, auth

--- allgoodrecipes/urls.py
+++ allgoodrecipes/urls.py
@@ -3,5 +3,13 @@ from allgoodrecipes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/', include([
+        path('', views.profile, name='profile'),
+        path('settings/', views.profile_settings),
+        path('publications/', views.profile_publications),
+        path('comments/', views.profile_comments),
+    ])),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
