469943b1b4438ecd66fa623d439624613f82723d
allumen <2356067R@student.gla.ac.uk>
2019-03-07 16:14:20 +0100

    basic url mapping

--- allgoodrecipes/urls.py
+++ allgoodrecipes/urls.py
@@ -3,4 +3,5 @@ from allgoodrecipes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/', views.profile, name='profile'),
 ]
--- wad_coursework/urls.py
+++ wad_coursework/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from allgoodrecipes import urls
-
+from allgoodrecipes import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('allgoodrecipes.urls')),
+    path('', include('allgoodrecipes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
