1ac15ccd3ed96d25b0c2eea23058459117a9ac06
allumen <2356067R@student.gla.ac.uk>
2019-03-07 16:25:20 +0100

    add profile views

--- allgoodrecipes/views.py
+++ allgoodrecipes/views.py
@@ -4,5 +4,28 @@ from django.http import HttpResponse, HttpResponseRedirect
 def index(request):
     return HttpResponse("index")
 
+@login_required
 def profile(request):
     return HttpResponse("profile")
+
+@login_required
+def profile_settings(request):
+    pass
+
+@login_required
+def profile_publications(request):
+    pass
+
+@login_required
+def profile_comments(request):
+    pass
+    
+def register(request):
+    pass
+    
+def login(request):
+    pass
+
+@login_required    
+def logout(request):
+    pass
