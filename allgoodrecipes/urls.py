5b299cd3848b5e91a3133c1313cf4be291014535
allumen <2356067R@student.gla.ac.uk>
2019-03-08 18:55:13 +0100

    add registration, login, logout, profile pages

--- allgoodrecipes/urls.py
+++ allgoodrecipes/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
         path('comments/', views.profile_comments),
     ])),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- allgoodrecipes/views.py
+++ allgoodrecipes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from allgoodrecipes.forms import UserForm, UserProfileForm
 
 def index(request):
     return HttpResponse("index")
 
+@login_required
 def profile(request):
     context_dict = {}
     return render(request, 'allgoodrecipes/profile.html', context=context_dict)
@@ -22,11 +26,84 @@ def profile_comments(request):
     pass
     
 def register(request):
-    pass
+    # tell the template if registration successful
+    registered = False
     
-def login(request):
-    pass
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            
+            user.set_password(user.password)
+            user.save()
+            
+            # commit = False to delay saving the model until we can avoid integrity problems
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+                
+            profile.save()
+            
+            # tell template registration is successfull
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        # not a POST request render registration form with blank ModelForm instances
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+        
+
+    context_dict = {'user_form': user_form,
+                     'profile_form': profile_form,
+                      'registration_complete': registered}
+                      
+    return render(request, 'allgoodrecipes/register.html', context=context_dict)
+    
+# *user_login* is used instead of *login* as the latter would shadow django.auth.login() function
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # if a user object returned, combaination valid
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            if user.is_active:
+                login(request, user)
+                # Login SUCCESSFUL
+                next_page = request.POST.get('redirect_url')
+                if next_page:
+                    return HttpResponseRedirect(next_page)
+                else:
+                    return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('Your account is disabled')
+        else:
+            error_message = ''
+            
+            try:
+                user = User.objects.get(username=username)
+                print("Invalid password for user {}: {}".format(username, password))
+                error_message = "Invalid password for user {}".format(username)
+            except User.DoesNotExist:
+                error_message = "User not found: {}".format(username)
+                
+            return render(request, 'allgoodrecipes/login.html', {'error_message': error_message})
+    else:
+        next_page = ''
+        try:
+            next_page = request.GET['next']
+        except KeyError:
+            pass
+        return render(request, 'allgoodrecipes/login.html', {'redirect_url':next_page})
 
 @login_required    
-def logout(request):
-    pass
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
