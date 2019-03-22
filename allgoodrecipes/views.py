be5c17abbf6fc5c17623db996bbe81182f7e705a
Karen Tada <2298995t@student.gla.ac.uk>
2019-03-22 17:28:59 +0000

    contact

--- allgoodrecipes/views.py
+++ allgoodrecipes/views.py
@@ -264,7 +264,7 @@ def sitemap(request, sitemaps, section=None,
     
     
 def contact(request):
-    return redirect('contact')
+    return render(request, 'allgoodrecipes/contact.html', {})
     
 def register(request):
     # tell the template if registration successful
